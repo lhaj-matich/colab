a8fb73e4e697a2b741d4b11b1de81adda53538df
lhaj-matich <choumouloussama@gmail.com>
2024-09-09 22:20:10 +0100

    feat: finished the header.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,9 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
+import { DM_Sans } from 'next/font/google'
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const dm = DM_Sans({ subsets: ["latin"]}) 
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -26,7 +18,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${dm.className} antialiased`}
       >
         {children}
       </body>
---
+++ src/sections/Header.tsx
@@ -0,0 +1,28 @@
+import Image from "next/image";
+import Logo from "@/assets/colab_logo.svg";
+
+const Header = () => {
+  return (
+    <header className="container mx-auto p-6">
+      <div className="flex justify-between items-center">
+        <div>
+          <Image src={Logo} alt="Logo" height={24} />
+        </div>
+        <nav className="flex gap-7 items-center">
+          <a href="#" className="link">Home</a>
+          <a href="#" className="link">Projects</a>
+          <a href="#" className="link">Servcies</a>
+          <a href="#" className="link">Blog</a>
+          <a href="#" className="link">About</a>
+          <a href="#" className="link">Contact</a>
+        </nav>
+        <div className="flex gap-4">
+          <button className="btn">Sign up</button>
+          <button className="btn btn-primary">Log in</button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
